3dec8fac4756a951eda50f531122c83689a801b2
MelnikVladyslav <programmergamingvlad@gmail.com>
2023-02-02 15:46:56 +0200

    Add Calculator

--- HomeWork 1/Calculator/src/main/java/org/example/Main.java
+++ HomeWork 1/Calculator/src/main/java/org/example/Main.java
@@ -1,7 +1,56 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Calculator");
+
+        int num1 = 0;
+        int num2 = 0;
+
+        System.out.println("Enter the number 1: ");
+        num1 = in.nextInt();
+        System.out.println("Enter the number 2: ");
+        num2 = in.nextInt();
+
+        int numOper = 0;
+        System.out.println("Enter the number operation: ");
+        System.out.println("1 - + ");
+        System.out.println("2 - - ");
+        System.out.println("3 - * ");
+        System.out.println("4 - / ");
+        numOper = in.nextInt();
+
+        int res = 0;
+        if (numOper == 1)
+        {
+            res = num1 + num2;
+            System.out.println("Result = " + res);
+        }
+        if (numOper == 2)
+        {
+            res = num1 - num2;
+            System.out.println("Result = " + res);
+        }
+        if (numOper == 3)
+        {
+            res = num1 * num2;
+            System.out.println("Result = " + res);
+        }
+        if (numOper == 4)
+        {
+            if (num2 != 0)
+            {
+                res = num1 / num2;
+                System.out.println("Result = " + res);
+            }
+            else
+            {
+                System.out.println("Na zero dilutu no can");
+            }
+        }
+
     }
 }
