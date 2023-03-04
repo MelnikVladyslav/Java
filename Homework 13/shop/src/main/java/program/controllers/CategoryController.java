5f6eb2d9bf840af459cc9a08093349cd0504c2e5
MelnikVladyslav <programmergamingvlad@gmail.com>
2023-03-04 14:23:58 +0200

    Add HomeWork 13

---
+++ Homework 13/shop/src/main/java/program/controllers/CategoryController.java
@@ -0,0 +1,57 @@
+package program.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import program.dto.category.CategoryCreateDTO;
+import program.dto.category.CategoryItemDTO;
+import program.dto.category.CategoryUpdateDTO;
+import program.entities.CategoryEntity;
+import program.mapper.CategoryMapper;
+import program.repositories.CategoryRepository;
+import program.storage.StorageService;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/categories")
+public class CategoryController {
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+    private final StorageService storageService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryItemDTO>> index() {
+        var list = categoryRepository.findAll();
+        var model = categoryMapper.CategoryItemsByCategories(list);
+        return new ResponseEntity<>(model, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<CategoryEntity> create(CategoryCreateDTO model) {
+        var image = storageService.save(model.getBase64());
+        CategoryEntity category = categoryMapper.CategoryByCreateCategoryDTO(model);
+        category.setImage(image);
+        categoryRepository.save(category);
+        return new ResponseEntity<>(category, HttpStatus.CREATED);
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<CategoryEntity> get(@PathVariable("id") Integer categoryId) {
+        var extCategory = categoryRepository.findById(categoryId);
+        return new ResponseEntity<>(extCategory.get(), HttpStatus.OK);
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<CategoryEntity> update(@PathVariable("id") Integer categoryId,
+                                                 CategoryUpdateDTO model) {
+        CategoryEntity category = categoryRepository.findById(categoryId).get();
+        category.setName(model.getName());
+        var upatedCategory = categoryRepository.save(category);
+        return new ResponseEntity<>(upatedCategory, HttpStatus.OK);
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") Integer categoryId) {
+       categoryRepository.deleteById(categoryId);
+        return new ResponseEntity<>("Cateogory Delete", HttpStatus.OK);
+    }
+}
---
+++ Homework 13/shop/src/main/java/program/dto/category/CategoryCreateDTO.java
@@ -0,0 +1,9 @@
+package program.dto.category;
+import lombok.Data;
+
+@Data
+public class CategoryCreateDTO {
+    private String name;
+    private String base64;
+    private String description;
+}
